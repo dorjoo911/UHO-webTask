32a0505eaea9def11752df59d40c6ff0f036cdd9
dorjoo911 <73855281+dorjoo911@users.noreply.github.com>
2024-01-22 09:51:12 -0500

    Update BootstrapChallenge.js

--- src/screens/bootstrap-challenge/BootstrapChallenge.js
+++ src/screens/bootstrap-challenge/BootstrapChallenge.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import CustomCarousel from "../../components/custom-carousel/CustomCarousel";
 import CustomAccordion from "../../components/custom-accordion/CustomAccordion";
 
